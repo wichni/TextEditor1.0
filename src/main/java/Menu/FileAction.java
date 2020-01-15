7c10da083d6d8453abed74bb812acbaef38fe93e
pmisko <miskopiotr@gmail.com>
2020-01-15 16:10:13 +0100

    File menu items with action

---
+++ src/main/java/Menu/FileAction.java
@@ -0,0 +1,52 @@
+package Menu;
+
+import javax.swing.*;
+import java.awt.print.PrinterException;
+import java.io.*;
+
+class FileAction {
+
+    static void clearAction(JTextArea textArea) {
+        textArea.setText("");
+    }
+
+    static void printAction(JTextArea textArea) {
+        try {
+            textArea.print();
+        } catch (PrinterException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    static void saveFileAction(JTextArea textArea, JFileChooser fileChooser) {
+        int choose = fileChooser.showSaveDialog(null);
+        if (choose == JFileChooser.APPROVE_OPTION) {
+            final File selectedFile = fileChooser.getSelectedFile();
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(selectedFile));
+                writer.write(textArea.getText());
+                writer.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    static void openFileAction(JTextArea textArea, JFileChooser fileChooser) {
+        int choose = fileChooser.showOpenDialog(null);
+        if (choose == JFileChooser.APPROVE_OPTION) {
+            File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                StringBuilder textFromFile = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    textFromFile.append(line);
+                }
+                textArea.append(textFromFile.toString());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
