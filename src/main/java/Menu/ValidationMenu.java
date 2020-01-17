e32a05f2d7c3a647257edce4739f34bebb783939
Jakub Wichniarek <jakubwichniarek@yahoo.pl>
2020-01-17 18:03:54 +0100

    validationMenu

--- src/main/java/Menu/ValidationMenu.java
+++ src/main/java/Menu/ValidationMenu.java
@@ -9,7 +9,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ValidationMenu extends JPanel implements ActionListener {
-    JsonValidator jsonValidator;
     private JPanel radioPanel;
     ButtonGroup buttonGroup;
     JRadioButton jsonButton;
