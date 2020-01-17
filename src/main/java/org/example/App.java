e2ff23220814e2d070bd5a8b179e981caf494234
Jakub Wichniarek <jakubwichniarek@yahoo.pl>
2020-01-17 18:32:54 +0100

    empty

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,11 @@
 package org.example;
 
-import myFrame.MyFrame;
-
 import javax.swing.*;
 
 import static java.awt.EventQueue.invokeLater;
 
 public class App extends JFrame {
-
     public static void main(String[] args) {
-        invokeLater(new Runnable() {
-            public void run() {
-                new MyFrame();
-            }
-        });
+
     }
 }
