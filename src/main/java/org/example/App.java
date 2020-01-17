40754e244b451ef0182b04bfdcf1fbab01010af3
Jakub Wichniarek <jakubwichniarek@yahoo.pl>
2020-01-17 18:42:58 +0100

    adasd

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+import javax.swing.*;
+
+import static javax.swing.SwingUtilities.invokeLater;
+
+public class App extends JFrame {
+
+    public static void main(String[] args) {
+        invokeLater(new Runnable() {
+            @Override
+            public void run() {
+            }
+        });
+    }
+}
