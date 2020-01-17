3e35946fb31222fa276b207da85d983373b75159
pmisko <miskopiotr@gmail.com>
2020-01-17 15:17:30 +0100

    File, edit popUp menu, url and json validation completed

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,9 +4,15 @@ import myFrame.MyFrame;
 
 import javax.swing.*;
 
+import static java.awt.EventQueue.invokeLater;
+
 public class App extends JFrame {
 
     public static void main(String[] args) {
-        new MyFrame();
+        invokeLater(new Runnable() {
+            public void run() {
+                new MyFrame();
+            }
+        });
     }
 }
